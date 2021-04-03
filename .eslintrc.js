e801b5964d6ba693f31938fafca9f62d1248a4ac
shkrmkr <77891053+shkrmkr@users.noreply.github.com>
2021-04-03 20:47:24 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    node: 'true',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Vue3 + Vite + TypeScript + ESLint + Prettier Template
+
+## VS Code Settings
+
+Vetur를 사용한다면:
+```
+"eslint.validate": [
+  "vue"
+],
+"vetur.validation.template": false,
+```
+
+저장 시 format 기능을 사용하려면:
+```
+"editor.codeActionsOnSave": {
+  "source.fixAll.eslint": true
+},
+```
